c938eeb746da06cd4189e476cf5a00e68c42fcae
Ziyin Huang <ziyinh@google.com>
2024-04-08 15:15:32 -0700

    Add new sets of XlaSparseDenseMatmulWithStaticBufferSizeOps that takes a static buffer size as an attribute.
    PiperOrigin-RevId: 622965124

--- tensorflow/core/tpu/kernels/sparse_core_preprocess_ops.h
+++ tensorflow/core/tpu/kernels/sparse_core_preprocess_ops.h
@@ -146,6 +146,111 @@ class StoreMinibatchStatisticsInFdoOp : public OpKernel {
   int64_t num_sc_per_chip_;
 };
 
+// TODO(pineapplejuice233): Unify this op with ConvertToListOfCooTensorsV2Op.
+class ConvertToListOfSparseCoreCooTensorsOp : public OpKernel {
+ public:
+  explicit ConvertToListOfSparseCoreCooTensorsOp(OpKernelConstruction* ctx);
+  ~ConvertToListOfSparseCoreCooTensorsOp() override = default;
+  ConvertToListOfSparseCoreCooTensorsOp(
+      const ConvertToListOfSparseCoreCooTensorsOp&) = delete;
+  ConvertToListOfSparseCoreCooTensorsOp& operator=(
+      const ConvertToListOfSparseCoreCooTensorsOp&) = delete;
+
+  void Compute(OpKernelContext* ctx) override;
+
+ private:
+  void WriteToOutputTensor(int32* row_ids, int32* col_ids, float* gains,
+                           int32* row_ids_tensor_ptr, int32* col_ids_tensor_ptr,
+                           float* gains_tensor_ptr, int32_t begin_index,
+                           int32_t end_index, int32_t sc_id,
+                           std::optional<std::vector<float>> gains_rescale);
+  int sample_count_ = 1;
+  int num_sc_per_chip_ = 1;
+  int per_sc_sample_count_ = 1;
+  int row_offset_ = 0;
+  int col_offset_ = 0;
+  int col_shift_ = 0;
+  int num_sc_shards_;
+  int stacked_table_sample_count_;
+  int num_sc_shards_bit_mod_;
+  int num_sc_shards_bit_mod_inv_;
+  int per_sc_row_offset_;
+  int per_sc_stacked_table_sample_count_;
+  std::string combiner_;
+};
+
+class SortListOfSparseCoreCooTensorsOp : public OpKernel {
+ public:
+  explicit SortListOfSparseCoreCooTensorsOp(OpKernelConstruction* ctx);
+  ~SortListOfSparseCoreCooTensorsOp() override = default;
+  SortListOfSparseCoreCooTensorsOp(const SortListOfSparseCoreCooTensorsOp&) =
+      delete;
+  SortListOfSparseCoreCooTensorsOp& operator=(
+      const SortListOfSparseCoreCooTensorsOp&) = delete;
+
+  void Compute(OpKernelContext* ctx) override;
+
+ private:
+  int num_sc_per_chip_ = 1;
+  int feature_width_ = 1;
+  int num_replica_ = 1;
+  int32_t num_physical_replica_ = 1;
+  int32_t num_physical_replica_bit_ = 1;
+  int32_t max_ids_per_sparse_core_;
+  int32_t max_unique_ids_per_sparse_core_;
+  std::string table_name_;
+  std::vector<int> sample_count_list_;
+  std::vector<int> col_offset_list_;
+  std::map<int32_t, std::vector<int32_t>> col_offset_to_feature_id_;
+};
+
+class ConvertToSparseCoreCsrWrappedCooTensorOp : public OpKernel {
+ public:
+  explicit ConvertToSparseCoreCsrWrappedCooTensorOp(OpKernelConstruction* ctx);
+  ~ConvertToSparseCoreCsrWrappedCooTensorOp() override = default;
+  ConvertToSparseCoreCsrWrappedCooTensorOp(
+      const ConvertToSparseCoreCsrWrappedCooTensorOp&) = delete;
+  ConvertToSparseCoreCsrWrappedCooTensorOp& operator=(
+      const ConvertToSparseCoreCsrWrappedCooTensorOp&) = delete;
+
+  void Compute(OpKernelContext* ctx) override;
+
+ private:
+  int num_sc_per_chip_ = 1;
+  int table_vocab_size_ = 1;
+  int feature_width_ = 1;
+  int num_replica_ = 1;
+  int sample_count_per_sc_ = 1;
+  int max_minibatches_per_sc_ = 1;
+  int max_ids_per_chip_per_sample_ = 1;
+  bool allow_id_dropping_;
+  std::string table_name_;
+  std::string device_name_;
+};
+
+class GetStatsFromListOfSparseCoreCooTensorsOp : public OpKernel {
+ public:
+  explicit GetStatsFromListOfSparseCoreCooTensorsOp(OpKernelConstruction* ctx);
+  ~GetStatsFromListOfSparseCoreCooTensorsOp() override = default;
+  GetStatsFromListOfSparseCoreCooTensorsOp(
+      const GetStatsFromListOfSparseCoreCooTensorsOp&) = delete;
+  GetStatsFromListOfSparseCoreCooTensorsOp& operator=(
+      const GetStatsFromListOfSparseCoreCooTensorsOp&) = delete;
+
+  void Compute(OpKernelContext* ctx) override;
+
+ private:
+  int num_sc_per_chip_ = 1;
+  int feature_width_ = 1;
+  int num_replica_ = 1;
+  int32_t num_physical_replica_ = 1;
+  int32_t num_physical_replica_bit_ = 1;
+  std::string table_name_;
+  std::vector<int> sample_count_list_;
+  std::vector<int> col_offset_list_;
+  std::map<int32_t, std::vector<int32_t>> col_offset_to_feature_id_;
+};
+
 }  // namespace tensorflow
 
 #endif  // TENSORFLOW_CORE_TPU_KERNELS_SPARSE_CORE_PREPROCESS_OPS_H_
